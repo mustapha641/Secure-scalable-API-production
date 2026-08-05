ea8059dab0f4d86f4bc5d300a677d63994ea7ac5
Mustapha <momohmustapha641@gmail.com>
2026-08-05 23:33:12 +0100

    implement eslint

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to the Secure Scalable API");
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the Secure Scalable API');
 });
 export default app;
